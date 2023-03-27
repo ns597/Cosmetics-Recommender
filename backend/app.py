3bcc60ed3917671cb6a1b43074a2174c0419c650
cathli66 <cl893@cornell.edu>
2023-03-27 16:17:24 -0400

    updated backend to return recommended routine based on ingreds only

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
+from helpers.similarity import top5category
 
 # ROOT_PATH for linking with all your files. 
 # Feel free to use a config.py or settings.py with a global export variable
@@ -27,17 +28,33 @@ CORS(app)
 # Sample search, the LIKE operator in this case is hard-coded, 
 # but if you decide to use SQLAlchemy ORM framework, 
 # there's a much better and cleaner way to do this
-def sql_search(episode):
-    query_sql = f"""SELECT * FROM episodes WHERE LOWER( title ) LIKE '%%{episode.lower()}%%' limit 10"""
-    keys = ["id","title","descr"]
-    data = mysql_engine.query_selector(query_sql)
-    return json.dumps([dict(zip(keys,i)) for i in data])
+def sql_search(prod):
+    # query_sql = f"""SELECT * FROM cosmetics WHERE LOWER( title ) LIKE '%%{prod.lower()}%%' limit 10"""
+    query_sql = f"""SELECT Ingredients FROM cosmetics WHERE LOWER( title ) LIKE '%%{prod.lower()}%%' limit 1"""
+    query_ingreds = mysql_engine.query_selector(query_sql)[0]
+
+    m_query = f"""SELECT Name, Ingredients FROM cosmetics WHERE Label = 'Moisturizer'"""
+    moisturizers = mysql_engine.query_selector(m_query)
+    c_query = f"""SELECT Name, Ingredients FROM cosmetics WHERE Label = 'Cleanser'"""
+    cleansers = mysql_engine.query_selector(c_query)
+    s_query = f"""SELECT Name, Ingredients FROM cosmetics WHERE Label = 'Sunscreen'"""
+    sunscreens = mysql_engine.query_selector(s_query)
+    t_query = f"""SELECT Name, Ingredients FROM cosmetics WHERE Label = 'Treatment'"""
+    treatments = mysql_engine.query_selector(t_query)
+
+    routine = {}
+    routine["Moisturizer"] = top5category(moisturizers, query_ingreds)
+    routine["Cleanser"] = top5category(cleansers, query_ingreds)
+    routine["Sunscreen"] = top5category(sunscreens, query_ingreds)
+    routine["Treatment"] = top5category(treatments, query_ingreds)
+    return json.dumps(routine)
+
 
 @app.route("/")
 def home():
     return render_template('base.html',title="sample html")
 
-@app.route("/episodes")
+@app.route("/products")
 def episodes_search():
     text = request.args.get("title")
     return sql_search(text)
--- backend/helpers/similarity.py
+++ backend/helpers/similarity.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
 
-def top5category(category,product):
-    #returns top 5 given a list and product to match
+def top5category(category,ingredients):
+    #returns top 5 given a list and ingredients to match
     #go through whole list and get name + ingredient list
     #perform jaccard similarity and put into list of tuples (Name, Score)
     #sort by score and return top 5
-    category = category[['Name', 'Ingredients']]
+
+    # category = category[['Name', 'Ingredients']]
     category = category.to_numpy()
-    ingredients = product['Ingredients']
     scores = []
     for val in category:
         name = val[0]
@@ -17,7 +17,7 @@ def top5category(category,product):
     scores.sort(key=lambda x:x[1])
     top5 = scores[:5]
     top5 = list(map(lambda x:x[0],top5))
-    print(top5)
+    return top5
 
 
 def jaccard_similarity(ingred, products):
@@ -48,12 +48,12 @@ treatment = skin_type[skin_type['Label']=='Treatment']
 first = moisturizers['Ingredients'][0]
 
 print("Moisturizers")
-top5category(moisturizers, product)
+top5category(moisturizers, product["Ingredients"])
 print("Cleansers")
-top5category(cleansers, product)
+top5category(cleansers, product["Ingredients"])
 print("Sunscreen")
-top5category(sunscreen, product)
+top5category(sunscreen, product["Ingredients"])
 print("Treatment")
-top5category(treatment, product)
+top5category(treatment, product["Ingredients"])
 
 
