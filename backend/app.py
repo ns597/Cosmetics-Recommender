6686c4e1f810d53effb7609837f806f88c917d7a
cl893 <--global>
2023-03-27 17:59:49 -0400

    changed all db names back

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 MYSQL_USER = "root"
 MYSQL_USER_PASSWORD = "admin"
 MYSQL_PORT = 3306
-MYSQL_DATABASE = "cosmetics"
+MYSQL_DATABASE = "kardashiandb"
 
 mysql_engine = MySQLDatabaseHandler(
     MYSQL_USER, MYSQL_USER_PASSWORD, MYSQL_PORT, MYSQL_DATABASE)
--- init.sql
+++ init.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `products`
 --
 
+USE kardashaindb;
+
 DROP TABLE IF EXISTS `products`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
