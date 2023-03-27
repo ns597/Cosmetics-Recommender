1f2e26ad2e600f7368fab1f3282745cbf25f4b16
cl893 <--global>
2023-03-27 19:43:07 -0400

    got rid of to_numpy() for now

--- backend/helpers/similarity.py
+++ backend/helpers/similarity.py
@@ -1,19 +1,19 @@
-def top5category(category,ingredients):
-    #returns top 5 given a list and ingredients to match
-    #go through whole list and get name + ingredient list
-    #perform jaccard similarity and put into list of tuples (Name, Score)
-    #sort by score and return top 5
+def top5category(category, ingredients):
+    # returns top 5 given a list and ingredients to match
+    # go through whole list and get name + ingredient list
+    # perform jaccard similarity and put into list of tuples (Name, Score)
+    # sort by score and return top 5
 
     # category = category[['Name', 'Ingredients']]
-    category = category.to_numpy()
+    # category = category.to_numpy()
     scores = []
     for val in category:
         name = val[0]
         score = jaccard_similarity(val[1], ingredients)
         scores = scores + [(name, score)]
-    scores.sort(key=lambda x:x[1])
+    scores.sort(key=lambda x: x[1])
     top5 = scores[:5]
-    top5 = list(map(lambda x:x[0],top5))
+    top5 = list(map(lambda x: x[0], top5))
     return top5
 
 
@@ -26,12 +26,11 @@ def jaccard_similarity(ingred, product):
     return (len(intersection) / len(union))
 
 
-
-# cosmetics = pd.read_csv('cosmetics.csv')  
+# cosmetics = pd.read_csv('cosmetics.csv')
 # # print(cosmetics)
 
 # #Types of input to initalize
-# #limit to skin type first, then perform jaccard similarity on three groups 
+# #limit to skin type first, then perform jaccard similarity on three groups
 # skin_type_input = input("Input your skin type: ")
 # prod_name = input("Input the name of product you have used an enjoyed: ")
 
@@ -53,5 +52,3 @@ def jaccard_similarity(ingred, product):
 # top5category(sunscreen, product["Ingredients"])
 # print("Treatment")
 # top5category(treatment, product["Ingredients"])
-
-
