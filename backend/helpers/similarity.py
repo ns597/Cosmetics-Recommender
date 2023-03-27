332f581f7d1b0410262aa05a16e28a82ad649816
cl893 <--global>
2023-03-27 16:31:09 -0400

    removed numpy and pandas

--- backend/helpers/similarity.py
+++ backend/helpers/similarity.py
@@ -1,6 +1,3 @@
-import pandas as pd
-import numpy as np
-
 def top5category(category,ingredients):
     #returns top 5 given a list and ingredients to match
     #go through whole list and get name + ingredient list
@@ -30,31 +27,31 @@ def jaccard_similarity(ingred, product):
 
 
 
-cosmetics = pd.read_csv('cosmetics.csv')  
-# print(cosmetics)
+# cosmetics = pd.read_csv('cosmetics.csv')  
+# # print(cosmetics)
 
-#Types of input to initalize
-#limit to skin type first, then perform jaccard similarity on three groups 
-skin_type_input = input("Input your skin type: ")
-prod_name = input("Input the name of product you have used an enjoyed: ")
+# #Types of input to initalize
+# #limit to skin type first, then perform jaccard similarity on three groups 
+# skin_type_input = input("Input your skin type: ")
+# prod_name = input("Input the name of product you have used an enjoyed: ")
 
-skin_type = cosmetics[cosmetics[skin_type_input.capitalize()]==1]
-product = cosmetics[cosmetics['Name']== prod_name]
+# skin_type = cosmetics[cosmetics[skin_type_input.capitalize()]==1]
+# product = cosmetics[cosmetics['Name']== prod_name]
 
-moisturizers = skin_type[skin_type['Label']=='Moisturizer']
-cleansers = skin_type[skin_type['Label']=='Cleanser']
-sunscreen =skin_type[skin_type['Label']=='Sun protect']
-treatment = skin_type[skin_type['Label']=='Treatment']
+# moisturizers = skin_type[skin_type['Label']=='Moisturizer']
+# cleansers = skin_type[skin_type['Label']=='Cleanser']
+# sunscreen =skin_type[skin_type['Label']=='Sun protect']
+# treatment = skin_type[skin_type['Label']=='Treatment']
 
-first = moisturizers['Ingredients'][0]
+# first = moisturizers['Ingredients'][0]
 
-print("Moisturizers")
-top5category(moisturizers, product["Ingredients"])
-print("Cleansers")
-top5category(cleansers, product["Ingredients"])
-print("Sunscreen")
-top5category(sunscreen, product["Ingredients"])
-print("Treatment")
-top5category(treatment, product["Ingredients"])
+# print("Moisturizers")
+# top5category(moisturizers, product["Ingredients"])
+# print("Cleansers")
+# top5category(cleansers, product["Ingredients"])
+# print("Sunscreen")
+# top5category(sunscreen, product["Ingredients"])
+# print("Treatment")
+# top5category(treatment, product["Ingredients"])
 
 
